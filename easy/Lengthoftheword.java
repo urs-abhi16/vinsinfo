6cae985591aefd5f43d4273dc9d74a2d3891026d
Abhinav S <abhinav.1611.s@gmail.com>
2025-12-09 10:18:34 +0530

    Add Lengthoftheword.java to calculate word length

---
+++ easy/Lengthoftheword.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class lengthofword {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the word: ");
+        String n=sc.nextLine();
+        int a=n.length();
+        System.out.println(a);
+        
+    }
+}
