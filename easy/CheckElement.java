53f073218b002a8188906ac1f0b273612b6a9e40
Abhinav S <abhinav.1611.s@gmail.com>
2025-12-09 10:23:41 +0530

    Create CheckElement.java

---
+++ easy/CheckElement.java
@@ -0,0 +1,15 @@
+public class CheckElement {
+    public static boolean containsElement(int[] arr, int target) {
+        for (int num : arr) {
+            if (num == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(containsElement(new int[]{1, 2, 3, 4}, 3)); 
+        System.out.println(containsElement(new int[]{1, 2, 3, 4}, 5));  
+        System.out.println(containsElement(new int[]{5, 10, 15}, 10));  
+    }
+}
